6895856ea72bef4fd37de95d1e7c724fb0290e4a
Nitesh <112567788+theniteshsolanki@users.noreply.github.com>
2025-01-05 14:33:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+#IMPORTING LIBRARIES AND FUNCTIONS
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+#Data collection and storing
+sonar_data = pd.read_csv("sonar-data.csv", header=None) #since are data file have no header we use header = none 
+
+print(sonar_data.head())#first five rows of data
 
